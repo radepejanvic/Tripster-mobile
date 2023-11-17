ed061b6906dcff5bed5eeac1b8aefb9752acb08a
Rade Pejanović <radepejanovic@gmail.com>
2023-11-17 13:57:24 +0100

    feat: Created MainActivity from bottom navigation template

---
+++ Tripster/app/src/main/java/com/example/tripster/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.example.tripster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int SPLASH_TIME_OUT = 1000;
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
