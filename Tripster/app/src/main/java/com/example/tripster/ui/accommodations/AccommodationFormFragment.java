ab7231ffa1ab31f286f863ee12a5d4e5058ff6c8
Rade Pejanović <radepejanovic@gmail.com>
2023-12-24 14:57:11 +0100

    feat: Created Accommodation form fragment

---
+++ Tripster/app/src/main/java/com/example/tripster/ui/accommodations/AccommodationFormFragment.java
@@ -0,0 +1,38 @@
+package com.example.tripster.ui;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tripster.R;
+
+public class AccommodationFormFragment extends Fragment {
+
+    private AccommodationFormViewModel mViewModel;
+
+    public static AccommodationFormFragment newInstance() {
+        return new AccommodationFormFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_accommodation_form, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(AccommodationFormViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Tripster/app/src/main/java/com/example/tripster/ui/accommodations/AccommodationFormViewModel.java
@@ -0,0 +1,7 @@
+package com.example.tripster.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class AccommodationFormViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
