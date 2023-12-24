fb706be7495dd1790354768c066aa564aa919ece
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-24 23:00:03 +0100

    feat: Created Day

---
+++ Tripster/app/src/main/java/com/example/tripster/model/Day.java
@@ -0,0 +1,37 @@
+package com.example.tripster.model;
+
+import java.time.LocalDate;
+
+
+public class Day {
+
+    private Long id;
+
+    private LocalDate date;
+
+    private double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
