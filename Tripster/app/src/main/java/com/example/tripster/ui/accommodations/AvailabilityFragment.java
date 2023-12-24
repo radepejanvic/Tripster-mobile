20ed9ff81fc3319b300fa41cf49c69367ea46f61
Rade Pejanović <radepejanovic@gmail.com>
2023-12-24 21:49:20 +0100

    temp: Binded text from VM to label

--- Tripster/app/src/main/java/com/example/tripster/ui/accommodations/AvailabilityFragment.java
+++ Tripster/app/src/main/java/com/example/tripster/ui/accommodations/AvailabilityFragment.java
@@ -1,5 +1,8 @@
 package com.example.tripster.ui.accommodations;
 
+import static androidx.navigation.ViewKt.findNavController;
+
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -11,28 +14,107 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.tripster.R;
+import com.example.tripster.databinding.FragmentAccommodationFormBinding;
+import com.example.tripster.databinding.FragmentAvailabilityBinding;
 
 public class AvailabilityFragment extends Fragment {
 
-    private AvailabilityViewModel mViewModel;
+    private FragmentAvailabilityBinding binding;
+    private ScrollView scrollView;
+    private Spinner mode;
+    private EditText price;
+    private Button add;
+    private Button update;
+    private Button disable;
 
-    public static AvailabilityFragment newInstance() {
-        return new AvailabilityFragment();
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_availability, container, false);
+
+        AvailabilityViewModel availabilityViewModel =
+                new ViewModelProvider(this).get(AvailabilityViewModel.class);
+
+        binding = FragmentAvailabilityBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        scrollView = binding.scroll;
+        price = binding.price;
+        mode = binding.mode;
+        add = binding.addPriceList;
+        update = binding.updatePriceList;
+        disable = binding.removePriceList;
+
+        spinnerSetUp(mode, R.array.availability_mode_options);
+        availabilityViewModel.getText().observe(getViewLifecycleOwner(), price::setText);
+
+        return root;
     }
 
+    private void spinnerSetUp(Spinner spinner, int optionsResId) {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Handle item selection
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                if (position == 0) {
+                    // Handle the case where the first item is selected
+                } else {
+                    // Handle other selections
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Handle no selection
+            }
+        });
+
+        String[] options = getResources().getStringArray(optionsResId);
+
+        // Create a custom adapter to disable the first item
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                spinner.getContext(),
+                android.R.layout.simple_spinner_item,
+                options
+        ) {
+            @Override
+            public boolean isEnabled(int position) {
+                return position != 0; // Disable the first item
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                if (position == 0) {
+                    // Disable the first item in the dropdown
+                    view.setEnabled(false);
+                    TextView textView = (TextView) view;
+                    textView.setTextColor(ContextCompat.getColor(getContext(), R.color.hint)); // Optionally change the color of disabled item
+                }
+                return view;
+            }
+        };
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+    }
+
+
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(AvailabilityViewModel.class);
-        // TODO: Use the ViewModel
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
 }
--- Tripster/app/src/main/java/com/example/tripster/ui/accommodations/AvailabilityViewModel.java
+++ Tripster/app/src/main/java/com/example/tripster/ui/accommodations/AvailabilityViewModel.java
@@ -1,7 +1,18 @@
 package com.example.tripster.ui.accommodations;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class AvailabilityViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private final MutableLiveData<String> price;
+
+    public AvailabilityViewModel() {
+        price = new MutableLiveData<>();
+        price.setValue("This is where the interval is displayed");
+    }
+
+    public LiveData<String> getText() {
+        return price;
+    }
 }
