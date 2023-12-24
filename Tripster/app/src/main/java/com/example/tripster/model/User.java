21d0a0f0296f114c9a5395f8892a4def31ee14ae
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-24 16:36:52 +0100

    feat: Added constructor with all arguments

--- Tripster/app/src/main/java/com/example/tripster/model/User.java
+++ Tripster/app/src/main/java/com/example/tripster/model/User.java
@@ -9,36 +9,37 @@ public class User {
 
     private String password;
 
-    public Long getUserID() {
-        return userID;
-    }
-
-    public void setUserID(Long userID) {
-        this.userID = userID;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Long getPersonID() {
-        return personID;
-    }
-
-    public void setPersonID(Long personID) {
-        this.personID = personID;
-    }
-
     private UserType userType;
-
+    private String name;
+    private String surname;
+    private String phone;
+    private String country;
+    private String city;
+    private String zipCode;
+    private String street;
+    private String number;
     private String token;
-
     private Long personID;
-    public User(long id, String email, UserType type,long personID) {
+
+    public User(){}
+
+    public User( String email, String password, UserType userType, String name,
+                 String surname, String phone, String country, String city,
+                 String zipCode, String street, String number) {
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.country = country;
+        this.city = city;
+        this.zipCode = zipCode;
+        this.street = street;
+        this.number = number;
+    }
+
+    public User(long id, String email, UserType type, long personID) {
         this.userID = id;
         this.email = email;
         this.userType = type;
@@ -50,17 +51,19 @@ public class User {
         this.password = password;
     }
 
-    public User(Long id, String jwt, Long person_id) {
-        this.userID = id;
-        this.token = jwt;
-        this.personID = person_id;
+
+    public Long getUserID() {
+        return userID;
     }
 
-
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
 
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
@@ -79,5 +82,85 @@ public class User {
 
     public void setUserType(UserType userType) {
         this.userType = userType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(Long personID) {
+        this.personID = personID;
     }
 }
