fc34af667d0b262deaeda0c369b5e6d980229722
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-24 12:35:30 +0100

    feat: Created SharedPreferencesManager

---
+++ Tripster/app/src/main/java/com/example/tripster/util/SharedPreferencesManager.java
@@ -0,0 +1,46 @@
+package com.example.tripster.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.tripster.model.User;
+import com.example.tripster.model.UserType;
+
+public class SharedPreferencesManager {
+    private static final String PREFERENCE_NAME = "MyAppPreferences";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_USER_ROLE = "user_role";
+    private static final String KEY_ID = "user_id";
+    private static final String PERSON_ID = "person_id";
+
+    private static SharedPreferences getSharedPreferences(Context context) {
+        return context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+    }
+
+    public static void saveUserInfo(Context context, String email, UserType userType, Long user_id,Long person_id) {
+        SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.putString(KEY_EMAIL, email);
+        editor.putString(KEY_USER_ROLE, userType.toString());
+        editor.putLong(KEY_ID, user_id);
+        editor.putLong(PERSON_ID, person_id);
+        editor.apply();
+    }
+
+    public static User getUserInfo(Context context) {
+        SharedPreferences preferences = getSharedPreferences(context);
+        String email = preferences.getString(KEY_EMAIL, "");
+        String userRole = preferences.getString(KEY_USER_ROLE, UserType.ADMIN.toString());
+        Long userid = preferences.getLong(KEY_ID, 0);
+        Long personId= preferences.getLong(PERSON_ID,0);
+        return new User( userid, email,  UserType.valueOf(userRole),personId);
+    }
+
+    public static void clearUserInfo(Context context) {
+        SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.remove(KEY_EMAIL);
+        editor.remove(KEY_USER_ROLE);
+        editor.remove(KEY_ID);
+        editor.remove(PERSON_ID);
+        editor.apply();
+    }
+}
