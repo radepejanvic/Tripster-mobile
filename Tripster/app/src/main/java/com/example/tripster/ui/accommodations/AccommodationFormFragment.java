dd2081fa34a9354b21df95279dde118a1989f913
Rade Pejanović <radepejanovic@gmail.com>
2023-12-24 16:24:28 +0100

    feat: Added bindings

--- Tripster/app/src/main/java/com/example/tripster/ui/accommodations/AccommodationFormFragment.java
+++ Tripster/app/src/main/java/com/example/tripster/ui/accommodations/AccommodationFormFragment.java
@@ -13,23 +13,63 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 
 import com.example.tripster.AuthorizationActivity;
 import com.example.tripster.MainActivity;
 import com.example.tripster.R;
+import com.example.tripster.databinding.FragmentAccommodationFormBinding;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AccommodationFormFragment extends Fragment {
 
     private static MainActivity ARG_PARAM1 = new MainActivity();
 
+    private FragmentAccommodationFormBinding binding;
+    private ScrollView scrollView;
+    private EditText name;
+    private EditText shortDescription;
+    private Spinner type;
+    private Spinner reservation;
+    private EditText minCap;
+    private EditText maxCap;
+    private EditText country;
+    private EditText city;
+    private EditText postalCode;
+    private EditText streetAndNumber;
+    private EditText description;
+    private Spinner pricePolicy;
+    private Spinner cancellationPolicy;
+
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_accommodation_form, container, false);
 
+        binding = FragmentAccommodationFormBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        scrollView = binding.scroll;
+        name = binding.name;
+        shortDescription = binding.shortDescription;
+        type = binding.type;
+        reservation = binding.reservationPolicy;
+        minCap = binding.minCap;
+        maxCap = binding.maxCap;
+        country = binding.country;
+        city = binding.city;
+        postalCode = binding.postalCode;
+        streetAndNumber = binding.streetAndNumber;
+        description = binding.description;
+        pricePolicy = binding.pricePolicy;
+        cancellationPolicy = binding.cancellationPolicy;
+
+        return root;
     }
 
     private void spinnerSetUp(Spinner spinner, ArrayList<String> options) {
