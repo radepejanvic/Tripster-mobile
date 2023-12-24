e139087f19261cb31139738165c2c87ad6b9d83d
Rade Pejanović <radepejanovic@gmail.com>
2023-12-24 18:19:19 +0100

    feat: Added getToolbarMenu

--- Tripster/app/src/main/java/com/example/tripster/MainActivity.java
+++ Tripster/app/src/main/java/com/example/tripster/MainActivity.java
@@ -14,7 +14,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private BottomNavigationView navigationBar;
     private UserType userType;
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +37,14 @@ public class MainActivity extends AppCompatActivity {
 
         navigationBar = binding.navBar;
 
-        userType = UserType.randomUserType();
-//        userType = UserType.ADMIN;
+//        userType = UserType.randomUserType();
+        userType = UserType.HOST;
 
 //        navigationBar.getMenu().findItem(R.id.navigation_reservations).setVisible(false);
 
         navigationBar.inflateMenu(getRoleMenu());
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        navController = Navigation.findNavController(this, R.id.nav_fragment_activity_main);
         NavigationUI.setupWithNavController(navigationBar, navController);
     }
 
@@ -58,10 +59,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private int getToolbarMenu() {
+        switch (userType) {
+            case ADMIN:
+                return R.menu.top_nav_menu_admin;
+            case HOST:
+                return R.menu.top_nav_menu_host;
+            default:
+                return R.menu.top_nav_menu_guest;
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.top_nav_menu, menu);
+        inflater.inflate(getToolbarMenu(), menu);
         return true;
     }
 
