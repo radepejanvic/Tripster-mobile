25b64263f9d47dfeb29b916f8efa60d758e0537c
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-24 12:36:32 +0100

    feat: Added check if user enlisted

--- Tripster/app/src/main/java/com/example/tripster/SplashScreenActivity.java
+++ Tripster/app/src/main/java/com/example/tripster/SplashScreenActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.WindowManager;
+
+import com.example.tripster.model.User;
+import com.example.tripster.util.SharedPreferencesManager;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,9 +22,18 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
+                User user = SharedPreferencesManager.getUserInfo(SplashScreenActivity.this);
+                if (user != null && !TextUtils.isEmpty(user.getEmail())) {
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    intent.putExtra("Role", user.getUserType().toString());
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
                 startActivity(new Intent(SplashScreenActivity.this, AuthorizationActivity.class));
                 finish();
             }
