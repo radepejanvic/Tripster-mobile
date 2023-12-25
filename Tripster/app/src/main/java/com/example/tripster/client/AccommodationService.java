a13f6a2e3b21b294573ec363c4d49d9fc3ca79fa
Rade Pejanović <radepejanovic@gmail.com>
2023-12-25 04:44:52 +0100

    feat: Added get and put requests

--- Tripster/app/src/main/java/com/example/tripster/client/AccommodationService.java
+++ Tripster/app/src/main/java/com/example/tripster/client/AccommodationService.java
@@ -5,8 +5,11 @@ import com.example.tripster.model.User;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface AccommodationService {
 
@@ -16,4 +19,19 @@ public interface AccommodationService {
     })
     @POST("accommodations")
     Call<Accommodation> save(@Body Accommodation accommodation);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json"
+    })
+    @GET("accommodations/{id}")
+    Call<Accommodation> getAccommodation(@Path("id") long id);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json"
+    })
+    @PUT("accommodations")
+    Call<Accommodation> updateAccommodation(@Body Accommodation accommodation);
+
 }
