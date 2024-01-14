dadc8fae1ff09131ad8e74e722f252f7b6069dda
Rade Pejanović <radepejanovic@gmail.com>
2024-01-15 00:13:00 +0100

    feat: Added basic navigation to reviews fragment

--- Tripster/app/src/main/java/com/example/tripster/AccommodationsFragment.java
+++ Tripster/app/src/main/java/com/example/tripster/AccommodationsFragment.java
@@ -18,11 +18,6 @@ import com.example.tripster.databinding.FragmentAccountBinding;
 import com.example.tripster.databinding.FragmentHomeBinding;
 import com.example.tripster.databinding.FragmentNotificationsBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AccommodationsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AccommodationsFragment extends Fragment {
 
     private FragmentAccommodationsBinding binding;
@@ -31,13 +26,7 @@ public class AccommodationsFragment extends Fragment {
 
         binding = FragmentAccommodationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        binding.hotel1012.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                findNavController(v).navigate(R.id.action_navigation_home_to_singleAccommodationFragment);
-
-            }
-        });
+        binding.hotel1012.setOnClickListener(v -> findNavController(v).navigate(R.id.action_navigation_home_to_singleAccommodationFragment));
 
         return root;
     }
--- Tripster/app/src/main/java/com/example/tripster/SingleAccommodationFragment.java
+++ Tripster/app/src/main/java/com/example/tripster/SingleAccommodationFragment.java
@@ -1,5 +1,7 @@
 package com.example.tripster;
 
+import static androidx.navigation.ViewKt.findNavController;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +9,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SingleAccommodationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.tripster.databinding.FragmentAccountBinding;
+import com.example.tripster.databinding.FragmentSingleAccommodationBinding;
+import com.example.tripster.util.SharedPreferencesManager;
+
 public class SingleAccommodationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SingleAccommodationFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SingleAccommodationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SingleAccommodationFragment newInstance(String param1, String param2) {
-        SingleAccommodationFragment fragment = new SingleAccommodationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FragmentSingleAccommodationBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_single_accommodation, container, false);
+
+        binding = FragmentSingleAccommodationBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        binding.reviews.setOnClickListener(v -> {
+            findNavController(v).navigate(R.id.action_singleAccommodationFragment_to_reviewFragment);
+        });
+
+
+        return root;
     }
 }
