1a06b7d04ef03cfb7a121531a193bffc1e35caf1
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-01-15 19:30:02 +0100

    feat: Added FragmentTransition to ReservationListFragment

--- Tripster/app/src/main/java/com/example/tripster/fragment/reservations/ReservationsFragment.java
+++ Tripster/app/src/main/java/com/example/tripster/fragment/reservations/ReservationsFragment.java
@@ -10,22 +10,30 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.tripster.FragmentTransition;
+import com.example.tripster.R;
 import com.example.tripster.databinding.FragmentReservationsBinding;
+import com.example.tripster.fragment.reviews.ReviewListFragment;
+import com.example.tripster.model.view.Reservation;
+import com.example.tripster.model.view.Review;
+
+import java.util.ArrayList;
 
 public class ReservationsFragment extends Fragment {
+
+    public static ArrayList<Reservation> reservations = new ArrayList<>();
 
     private FragmentReservationsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ReservationsViewModel reservationsViewModel =
-                new ViewModelProvider(this).get(ReservationsViewModel.class);
+
 
         binding = FragmentReservationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        reservationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        FragmentTransition.to(ReservationListFragment.newInstance(reservations), getActivity(), false, R.id.scroll_reviews_list);
+
         return root;
     }
 
