6542ae483ca4da6e9da09e368fbef15186d0733a
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-01-15 20:10:59 +0100

    feat: Added getReservationForGuest

--- Tripster/app/src/main/java/com/example/tripster/service/ReservationService.java
+++ Tripster/app/src/main/java/com/example/tripster/service/ReservationService.java
@@ -17,5 +17,10 @@ public interface ReservationService {
     })
     @GET("reservations/host/{id}")
     Call<List<Reservation>> getReservationForHost(@Path("id") long id);
-
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json"
+    })
+    @GET("reservations/guest/{id}")
+    Call<List<Reservation>> getReservationForGuest(@Path("id") long id);
 }
