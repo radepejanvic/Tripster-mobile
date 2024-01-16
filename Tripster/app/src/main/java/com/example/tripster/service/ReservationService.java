0edfef98e65735875c025d28ae2533739dc62a89
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-01-16 18:12:14 +0100

    feat: Added accept,reject,delete

--- Tripster/app/src/main/java/com/example/tripster/service/ReservationService.java
+++ Tripster/app/src/main/java/com/example/tripster/service/ReservationService.java
@@ -6,8 +6,10 @@ import com.example.tripster.model.view.Review;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ReservationService {
@@ -23,4 +25,23 @@ public interface ReservationService {
     })
     @GET("reservations/guest/{id}")
     Call<List<Reservation>> getReservationForGuest(@Path("id") long id);
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json"
+    })
+    @PUT("reservations/accept/{id}")
+    Call<String> accept(@Path("id") long id);
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json"
+    })
+    @PUT("reservations/reject/{id}")
+    Call<String> reject(@Path("id") long id);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json"
+    })
+    @DELETE("reservations/{id}")
+    Call<String> delete(@Path("id") long id);
 }
