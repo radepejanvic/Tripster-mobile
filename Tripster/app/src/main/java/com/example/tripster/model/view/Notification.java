4eed3e3efb9b34fc4e6b4733b8ec2ac78a77a77a
Rade Pejanović <radepejanovic@gmail.com>
2024-01-17 18:51:01 +0100

    feat: Created notification model parcelable view

---
+++ Tripster/app/src/main/java/com/example/tripster/model/view/Notification.java
@@ -0,0 +1,72 @@
+package com.example.tripster.model.view;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import com.example.tripster.model.enums.NotificationStatus;
+import com.example.tripster.model.enums.NotificationType;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Notification implements Parcelable {
+
+    private Long id;
+
+    private String title;
+
+    private String text;
+
+    private NotificationType type;
+
+    private NotificationStatus status;
+
+    private String timeStamp;
+
+    private Long userId;
+
+    protected Notification(Parcel in) {
+        id = in.readLong();
+        title = in.readString();
+        text = in.readString();
+        type = NotificationType.valueOf(in.readString());
+        status = NotificationStatus.valueOf(in.readString());
+        timeStamp = in.readString();
+        userId = in.readLong();
+    }
+
+    public static final Creator<Review> CREATOR = new Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel in) {
+            return new Review(in);
+        }
+
+        @Override
+        public Review[] newArray(int size) {
+            return new Review[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(title);
+        dest.writeString(text);
+        dest.writeString(String.valueOf(type));
+        dest.writeString(String.valueOf(status));
+        dest.writeString(timeStamp);
+        dest.writeLong(userId);
+    }
+
+}
